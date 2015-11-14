6285016a17600df66a7ec9d3430536c182a75fcd
Matthew Crawford <mjc5454@rit.edu>
2015-11-14 03:06:22 -0500

    adds functionality to test the average age for each movie, will probably be added somewhere else later

--- run.rb
+++ run.rb
@@ -3,6 +3,18 @@ require_relative 'movie'
 require_relative 'IMDB_Scraper'
 
 def run
-	movies = get_cast_list(get_movies_in_theaters)
-	
+	imdb_scraper = IMDB_Scraper.new
+	movies = imdb_scraper.get_cast_list(imdb_scraper.get_movies_in_theaters)
+	puts movies
+	sum = 0
+	count = 0 
+	movies.each do |movie|
+		movie.cast_list.each do |cast_member|
+			sum += cast_member.calculate_age
+			count += 1 
+		end
+		movie.average_age = sum/count
+		puts movie.average_age
+	end
 end
+run
