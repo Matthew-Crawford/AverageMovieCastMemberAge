30b5faafcd40ecbe3e10d1da5c2f6afdea95b72a
Matthew Crawford <mjc5454@rit.edu>
2015-11-14 03:10:11 -0500

    sets average_age default value to 0

--- movie.rb
+++ movie.rb
@@ -2,7 +2,7 @@ class Movie
 
 	attr_accessor :title, :movie_link, :average_age, :cast_list
 
-	def initialize(title, movie_link, average_age, cast_list=[])
+	def initialize(title, movie_link, average_age=0, cast_list=[])
 		@title = title
 		@movie_link = movie_link
 		@average_age = average_age
