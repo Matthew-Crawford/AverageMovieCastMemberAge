e0a67c6320636ae0c337d080560b66a97709b37c
Matthew Crawford <mjc5454@rit.edu>
2015-11-16 15:03:05 -0500

    corrects run.bat multi-line commands

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
-echo "Installing Required Gems"
-gem install mechanize
-gem install ruby-progressbar
-gem install command_line_reporter
-ruby ./display.rb
+echo "Installing Required Gems" & 
+gem install mechanize & 
+gem install ruby-progressbar & 
+gem install command_line_reporter & 
+ruby display.rb & 
+pause
