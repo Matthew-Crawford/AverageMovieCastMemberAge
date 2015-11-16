435d59b05f18fba3f38f949c3531525330a0adf5
Matthew Crawford <mjc5454@rit.edu>
2015-11-16 15:30:51 -0500

    fixes a divide by zero error

--- display.rb
+++ display.rb
@@ -1,3 +1,7 @@
+# display.rb
+# scrapes IMDB for all movies currently showing in theaters
+# and displays their associated average cast member age
+
 require_relative 'cast_member'
 require_relative 'movie'
 require_relative 'IMDB_Scraper'
@@ -5,12 +9,18 @@ require 'command_line_reporter'
 
 class Display
 	include CommandLineReporter
+
+	# runs IMDB_Scaper and displays the results
 	def run
+		min_cast_member_number = 3
+
 		imdb_scraper = IMDB_Scraper.new
 		movies = imdb_scraper.get_cast_list_for_each_movie(imdb_scraper.get_movies_in_theaters)
 		puts
+
 		header :title => "Average Age of Cast of Movies Currently in Theaters",
 			 :width => 100, :bold => true
+
 		table(:border => true) do
 			sum_age = 0
 			count = 0
@@ -18,9 +28,16 @@ class Display
 				column 'Title', :width => 30, :align => 'center'
 				column 'Average Age', :width => 25, :padding => 5
 			end
+
 			movies.each do |movie_thread|
 				movie = movie_thread.value
 				movie.average_age = movie.calculate_average_age
+
+				# skips the movie if the minimum cast_member number is
+				# not met
+				if movie.cast_list.count < min_cast_member_number
+					next
+				end
 				row do
 					column movie.title
 					column movie.average_age
@@ -28,6 +45,7 @@ class Display
 				sum_age += movie.average_age
 				count += 1
 			end	
+
 			average_age_all_movies = sum_age/count
 			row do
 				column 'Average Age of All Movies'
--- movie.rb
+++ movie.rb
@@ -17,13 +17,18 @@ class Movie
 	end
 
 	# calculates the average age of the cast
-	# @return: the average age the cast
+	# @return: the average age the cast, false if there are no cast members
+	# 		   with age an age	
 	def calculate_average_age
 		sum = 0
 		cast_list.each do |cast_member|
 			sum += cast_member.calculate_age
 		end
-		return sum/cast_list.count
+		if cast_list.count != 0
+			return sum/cast_list.count
+		else
+			return false
+		end
 	end
 
 	def display_average_age
