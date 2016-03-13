346d131c3a9005115670354fb68d822d25609fcd
Matthew Crawford <mcrawford0126@gmail.com>
2016-03-12 20:33:41 -0500

    changes indents to 2 spaces from tabs

--- IMDB_Scraper.rb
+++ IMDB_Scraper.rb
@@ -11,123 +11,123 @@ require_relative 'movie'
 
 # A web scraper that scrapes IMDB  
 class IMDB_Scraper
-	include CommandLineReporter
+  include CommandLineReporter
 
-	attr_accessor :scraper
+  attr_accessor :scraper
 
-	# Default url's 
-	BASE_URL = 'http://www.imdb.com/'
-	IN_THEATERS = 'http://www.imdb.com/movies-in-theaters'
+  # Default url's 
+  BASE_URL = 'http://www.imdb.com/'
+  IN_THEATERS = 'http://www.imdb.com/movies-in-theaters'
 
-	def initialize(scraper = Mechanize.new)
+  def initialize(scraper = Mechanize.new)
 
     @scraper = scraper
 
-		# sleeps for .4 seconds every time the scraper performs an action
-		 @scraper.history_added = Proc.new { sleep 0.4 }
-	end	
+    # sleeps for .4 seconds every time the scraper performs an action
+     @scraper.history_added = Proc.new { sleep 0.4 }
+  end 
 
 
-	# finds all movies currently in theaters
-	# @return: the urls of all movie pages currently shown in theaters
-	def get_movies_in_theaters
-		movie_links = []
+  # finds all movies currently in theaters
+  # @return: the urls of all movie pages currently shown in theaters
+  def get_movies_in_theaters
+    movie_links = []
 
-		scraper.get(IN_THEATERS) do |page|
-			movies_raw = page.search('td.overview-top')
+    scraper.get(IN_THEATERS) do |page|
+      movies_raw = page.search('td.overview-top')
 
-			movies_raw.each do |movie_link|
-				link = movie_link.search('h4').css('a')[0]
-				movie = Movie.new(link.attributes['title'].value, 
-					link.attributes['href'].value)
-				movie_links << movie
+      movies_raw.each do |movie_link|
+        link = movie_link.search('h4').css('a')[0]
+        movie = Movie.new(link.attributes['title'].value, 
+          link.attributes['href'].value)
+        movie_links << movie
 
-			end
-		end
-		return movie_links
-	end
+      end
+    end
+    return movie_links
+  end
 
-	# creates a thread for each movie currently showing in theaters 
-	# @param movies: the urls of all movie pages currently shown in theaters
-	# @return: a list of movies with all valid cast_members in it's cast_list attribute
-	def get_cast_list_for_each_movie(movies)
-		movie_list_threads = []
+  # creates a thread for each movie currently showing in theaters 
+  # @param movies: the urls of all movie pages currently shown in theaters
+  # @return: a list of movies with all valid cast_members in it's cast_list attribute
+  def get_cast_list_for_each_movie(movies)
+    movie_list_threads = []
 
-		puts "\nGetting cast for all movies currently in theaters\n\n"
-		progress_bar = ProgressBar.create( :format => '%a %bᗧ%i %p%%',
+    puts "\nGetting cast for all movies currently in theaters\n\n"
+    progress_bar = ProgressBar.create( :format => '%a %bᗧ%i %p%%',
                     :progress_mark  => ' ',
                     :remainder_mark => '･',
-                    :total 			=> movies.count)
+                    :total      => movies.count)
 
-		movies.each do |movie|
-			movie_list_threads << Thread.new {get_cast_list(movie, progress_bar)}
-		end
+    movies.each do |movie|
+      movie_list_threads << Thread.new {get_cast_list(movie, progress_bar)}
+    end
 
-		movie_list_threads.each do |thread|
-			thread.join 
-		end
+    movie_list_threads.each do |thread|
+      thread.join 
+    end
 
-		return movie_list_threads
-	end	
+    return movie_list_threads
+  end 
 
-	# gets the cast members and their date of birth's for each movie
-	# @param movie: the cast members' movie
-	# @return: the movie with the full cast member list
-	def get_cast_list(movie, progress_bar)
-		max_cast_members = 50
-		movie_page = scraper.get(BASE_URL + movie.movie_link)
-		full_cast_page = movie_page.link_with(text: 'See full cast').click
-		cast_member_count = 0
-		cast_list = full_cast_page.search('td.itemprop')
+  # gets the cast members and their date of birth's for each movie
+  # @param movie: the cast members' movie
+  # @return: the movie with the full cast member list
+  def get_cast_list(movie, progress_bar)
+    max_cast_members = 50
+    movie_page = scraper.get(BASE_URL + movie.movie_link)
+    full_cast_page = movie_page.link_with(text: 'See full cast').click
+    cast_member_count = 0
+    cast_list = full_cast_page.search('td.itemprop')
 
-		cast_list.each do |cast_member_section|
-			# stops after max_cast_members pages have been visited
-			cast_member_count += 1
-			if cast_member_count > max_cast_members
-				break
-			end
+    cast_list.each do |cast_member_section|
+      # stops after max_cast_members pages have been visited
+      cast_member_count += 1
+      if cast_member_count > max_cast_members
+        break
+      end
 
-			cast_member_link = cast_member_section.css('a')[0]
-			cast_member_name = cast_member_link.text
-			cast_member_dob = get_cast_member_dob(BASE_URL + cast_member_link.attributes['href'].value)
+      cast_member_link = cast_member_section.css('a')[0]
+      cast_member_name = cast_member_link.text
+      cast_member_dob = get_cast_member_dob(BASE_URL + cast_member_link.attributes['href'].value)
 
-			if(cast_member_dob != false)
-				date_of_birth = DateTime.new(cast_member_dob[2].to_i, cast_member_dob[0].to_i, cast_member_dob[1].to_i)
-				cast_member = Cast_Member.new(cast_member_name, cast_member_link, date_of_birth)
-				movie.cast_list << cast_member
-			end
-		end
-		progress_bar.increment
-		return movie
-	end
+      if(cast_member_dob != false)
+        date_of_birth = DateTime.new(cast_member_dob[2].to_i, cast_member_dob[0].to_i, cast_member_dob[1].to_i)
+        cast_member = Cast_Member.new(cast_member_name, cast_member_link, date_of_birth)
+        movie.cast_list << cast_member
+      end
+    end
+    progress_bar.increment
+    return movie
+  end
 
-	# goes to cast member's bio page and returns cast member's dob
-	# @param url: the directory path for the actor's bio page
-	# @return: an array in the format [day, month, year], or false if there isn't
-	# birthday listed
-	def get_cast_member_dob(url)
-		begin
-			# gets date of birth container
-			cast_member_page = scraper.get(url)
-			birthday_container = cast_member_page.search('#name-born-info')
+  # goes to cast member's bio page and returns cast member's dob
+  # @param url: the directory path for the actor's bio page
+  # @return: an array in the format [day, month, year], or false if there isn't
+  # birthday listed
+  def get_cast_member_dob(url)
+    begin
+      # gets date of birth container
+      cast_member_page = scraper.get(url)
+      birthday_container = cast_member_page.search('#name-born-info')
 
-			# formats the date of birth in 'mm d yyyy'
-			birthday = birthday_container.at('time').text.strip
-			birthday = birthday.gsub(/\r/, "")
-			birthday = birthday.gsub(/\n/, "")
-			birthday = birthday.split(" ")
-			birthday[1] = birthday[1].gsub(',', '')
-			birthday[0] = Date::MONTHNAMES.index(birthday[0]) 
+      # formats the date of birth in 'mm d yyyy'
+      birthday = birthday_container.at('time').text.strip
+      birthday = birthday.gsub(/\r/, "")
+      birthday = birthday.gsub(/\n/, "")
+      birthday = birthday.split(" ")
+      birthday[1] = birthday[1].gsub(',', '')
+      birthday[0] = Date::MONTHNAMES.index(birthday[0]) 
 
-			test_dob = DateTime.new(birthday[2].to_i, birthday[0].to_i, birthday[1].to_i)
+      test_dob = DateTime.new(birthday[2].to_i, birthday[0].to_i, birthday[1].to_i)
 
-			return birthday
+      return birthday
 
-		# catches if there is no date of birth for a given actor
-		rescue NoMethodError
-			return false
-		rescue ArgumentError
-			return false
-		end
-	end
+    # catches if there is no date of birth for a given actor
+    rescue NoMethodError
+      return false
+    rescue ArgumentError
+      return false
+    end
+  end
 end
--- display.rb
+++ display.rb
@@ -8,50 +8,50 @@ require_relative 'IMDB_Scraper'
 require 'command_line_reporter'
 
 class Display
-	include CommandLineReporter
+  include CommandLineReporter
 
-	# runs IMDB_Scaper and displays the results
-	def run
-		min_cast_member_number = 3
+  # runs IMDB_Scaper and displays the results
+  def run
+    min_cast_member_number = 3
 
-		imdb_scraper = IMDB_Scraper.new
-		movies = imdb_scraper.get_cast_list_for_each_movie(imdb_scraper.get_movies_in_theaters)
-		puts
+    imdb_scraper = IMDB_Scraper.new
+    movies = imdb_scraper.get_cast_list_for_each_movie(imdb_scraper.get_movies_in_theaters)
+    puts
 
-		header :title => "Average Age of the Cast Members of Movies Currently in Theaters",
+    header :title => "Average Age of the Cast Members of Movies Currently in Theaters",
            :width => 100, :bold => true
 
-		table(:border => true) do
-			sum_age = 0
-			count = 0
-			row  do
-				column 'Title', :width => 30, :align => 'center'
-				column 'Average Age', :width => 25, :padding => 5
-			end
+    table(:border => true) do
+      sum_age = 0
+      count = 0
+      row  do
+        column 'Title', :width => 30, :align => 'center'
+        column 'Average Age', :width => 25, :padding => 5
+      end
 
-			movies.each do |movie_thread|
-				movie = movie_thread.value
-				movie.average_age = movie.calculate_average_age
+      movies.each do |movie_thread|
+        movie = movie_thread.value
+        movie.average_age = movie.calculate_average_age
 
-				# skips the movie if the minimum cast_member number is
-				# not met
-				if movie.cast_list.count < min_cast_member_number
-					next
-				end
-				row do
-					column movie.title
-					column movie.average_age
-				end
-				sum_age += movie.average_age
-				count += 1
-			end	
+        # skips the movie if the minimum cast_member number is
+        # not met
+        if movie.cast_list.count < min_cast_member_number
+          next
+        end
+        row do
+          column movie.title
+          column movie.average_age
+        end
+        sum_age += movie.average_age
+        count += 1
+      end 
 
-			average_age_all_movies = sum_age/count
-			row do
-				column 'Average Age of All Movies'
-				column average_age_all_movies
-			end
-		end
-	end
+      average_age_all_movies = sum_age/count
+      row do
+        column 'Average Age of All Movies'
+        column average_age_all_movies
+      end
+    end
+  end
 end
 Display.new.run
